cc1ad6630f162a6f7d6e5c62350b4cff51742a61
smidmcwilliams <smidmcwilliams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 12:08:42 +0000

    tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/reach-connector/tags/1.4@1167641 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reach-connector.php
+++ reach-connector.php
@@ -8,7 +8,7 @@ Plugin Name: REACH Connector
 Plugin URI: http://wordpress.org/plugins/reach-connector/
 Description: This plugin enables you to easily integrate your REACH&#8480; campaign and sponsorships with your WordPress site. For more information on REACH&#8480; visit http://www.reachapp.co.
 Author: Sugar Maple Interactive, LLC
-Version: 0.2
+Version: 1.4
 Author URI: http://sugarmapleinteractive.com/code/wordpress/plugins/reach-connector
 Text Domain: reach
 License: GPLv2
@@ -63,13 +63,14 @@ if(!class_exists('Reach_Connector_Plugin')) {
       // Setting Sections : Section ID, Section Title, Callback, Page ID (Menu Slug)
       add_settings_section('section-one', 'REACH&#8480; Account Information', array($this, 'text_for_section_one'), 'reach-connector-options' );
     	// Setting Fields : Filed ID, Field Title, Callback, Page ID (Menu Slug), Section ID
-      add_settings_field( 'reach_api_host', 'Admin URL', array($this, 'field_for_api_host'), 'reach-connector-options', 'section-one' );
+      add_settings_field( 'reach_api_host', 'REACH&#8480; Account URL', array($this, 'field_for_api_host'), 'reach-connector-options', 'section-one' );
       add_settings_field( 'reach_account_guid', 'Account GUID', array($this, 'field_for_account_guid'), 'reach-connector-options', 'section-one' );
       add_settings_section('style-section', 'Style Options', array($this, 'text_for_style_section'), 'reach-connector-options' );
       add_settings_field( 'reach_sponsorship_class', 'Sponsorship CSS Class', array($this, 'field_for_sponsorship_classes'), 'reach-connector-options', 'style-section' );
       add_settings_field( 'reach_campaign_class', 'Campaign CSS Class', array($this, 'field_for_campaign_classes'), 'reach-connector-options', 'style-section' );
       add_settings_section('section-two', 'Sponsorship Shortcode Setup', array($this, 'text_for_section_two'), 'reach-connector-options' );
       add_settings_section('section-three', 'Campaign Shortcode Setup', array($this, 'text_for_section_three'), 'reach-connector-options' );
+      add_settings_section('section-four', 'Donation Shortcode Setup', array($this, 'text_for_section_four'), 'reach-connector-options' );
     }
 
     // Hook for WordPress admin_init action
@@ -121,7 +122,7 @@ if(!class_exists('Reach_Connector_Plugin')) {
     }
 
     public function text_for_section_one() {
-    	echo "Enter your REACH&#8480; Admin URL and Account GUID to setup the REACH&#8480; Connector plugin.";
+    	echo "Enter your REACH&#8480; Account URL and Account GUID to setup the REACH&#8480; Connector plugin.";
     }
     
     public function text_for_style_section() {
@@ -129,13 +130,19 @@ if(!class_exists('Reach_Connector_Plugin')) {
     }
     
     public function text_for_section_two() {
-    	echo "To pull a list of sponsorships from REACH&#8480; to display on your site use the shortcode [sponsorships]. You can also pass conditional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:";
+    	echo "To pull a list of sponsorships from REACH&#8480; to display on your site use the shortcode [sponsorships]. You can also pass optional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:";
       echo "<p><blockquote>sponsorship_type<br/>location<br/>project<br/>sponsorship_categories<br/>status</blockquote></p>";
       echo '<p>Example: [sponsorships sponsorship_type="children"]';
     }
     
     public function text_for_section_three() {
     	echo "To pull a list of campaigns from REACH&#8480; to display on your site use the shortcode [campaigns].";
+    }
+    
+    public function text_for_section_four() {
+    	echo "To display a donation page from REACH&#8480; on your site use the shortcode [donations] on any page. You can also pass optional paramters defined in the Giving Options page in REACH to customize the donation form by setting price, recurring period, purpose etc.";
+      echo "<p><blockquote>amount<br/>fixed_amount (true/false)<br/>recurring<br/>fixed_recurring (true/false)<br/>referral</blockquote></p>";
+      echo '<p>Example: [donations amount="50"]';
     }
 
   }
@@ -189,8 +196,29 @@ function get_campaigns() {
   echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
 }
 
+function get_donation_page($atts) {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $reach_campaign_class = esc_attr( get_option( 'reach_campaign_class' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $atts = shortcode_atts( array(
+      'amount' => '',
+      'fixed_amount' => '',
+      'recurring' => '',
+      'fixed_recurring' => '',
+      'referral' => '',
+      'disablenav' => 'true',
+  ), $atts, 'sponsorships' );
+  echo "<iframe id='donations-iframe' src='https://".str_replace($search, '', $reach_api_host)."/donations/new?".http_build_query($atts)."' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#donations-iframe").iFrameResize();</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
 add_shortcode('sponsorships', 'get_sponsorships');
 add_shortcode('campaigns', 'get_campaigns');
+add_shortcode('donations', 'get_donation_page');
 
 if(class_exists('Reach_Connector_Plugin')) {
   // WordPress hooks to activate and deactivate the plugin
