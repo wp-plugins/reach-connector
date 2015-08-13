13c7d71bb5c5c89db0903a28ba9c60654a03bcd9
smidmcwilliams <smidmcwilliams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 12:35:42 +0000

    tagging version 1.6
    git-svn-id: https://plugins.svn.wordpress.org/reach-connector/tags/1.6@1219951 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- reach-connector.php
+++ reach-connector.php
@@ -8,7 +8,7 @@ Plugin Name: REACH Connector
 Plugin URI: http://wordpress.org/plugins/reach-connector/
 Description: This plugin enables you to easily integrate your REACH&#8480; campaign and sponsorships with your WordPress site. For more information on REACH&#8480; visit http://www.reachapp.co.
 Author: Sugar Maple Interactive, LLC
-Version: 0.2
+Version: 1.6
 Author URI: http://sugarmapleinteractive.com/code/wordpress/plugins/reach-connector
 Text Domain: reach
 License: GPLv2
@@ -63,13 +63,19 @@ if(!class_exists('Reach_Connector_Plugin')) {
       // Setting Sections : Section ID, Section Title, Callback, Page ID (Menu Slug)
       add_settings_section('section-one', 'REACH&#8480; Account Information', array($this, 'text_for_section_one'), 'reach-connector-options' );
     	// Setting Fields : Filed ID, Field Title, Callback, Page ID (Menu Slug), Section ID
-      add_settings_field( 'reach_api_host', 'Admin URL', array($this, 'field_for_api_host'), 'reach-connector-options', 'section-one' );
+      add_settings_field( 'reach_api_host', 'REACH&#8480; Account URL', array($this, 'field_for_api_host'), 'reach-connector-options', 'section-one' );
       add_settings_field( 'reach_account_guid', 'Account GUID', array($this, 'field_for_account_guid'), 'reach-connector-options', 'section-one' );
       add_settings_section('style-section', 'Style Options', array($this, 'text_for_style_section'), 'reach-connector-options' );
       add_settings_field( 'reach_sponsorship_class', 'Sponsorship CSS Class', array($this, 'field_for_sponsorship_classes'), 'reach-connector-options', 'style-section' );
       add_settings_field( 'reach_campaign_class', 'Campaign CSS Class', array($this, 'field_for_campaign_classes'), 'reach-connector-options', 'style-section' );
       add_settings_section('section-two', 'Sponsorship Shortcode Setup', array($this, 'text_for_section_two'), 'reach-connector-options' );
-      add_settings_section('section-three', 'Campaign Shortcode Setup', array($this, 'text_for_section_three'), 'reach-connector-options' );
+      add_settings_section('section-three', 'Campaigns Shortcode Setup', array($this, 'text_for_section_three'), 'reach-connector-options' );
+      add_settings_section('section-four', 'Campaign Page Shortcode Setup', array($this, 'text_for_section_four'), 'reach-connector-options' );
+      add_settings_section('section-five', 'Donation Shortcode Setup', array($this, 'text_for_section_five'), 'reach-connector-options' );
+      add_settings_section('section-six', 'Projects Shortcode Setup', array($this, 'text_for_section_six'), 'reach-connector-options' );
+      add_settings_section('section-seven', 'Project Page Shortcode Setup', array($this, 'text_for_section_seven'), 'reach-connector-options' );
+      add_settings_section('section-eight', 'Places Shortcode Setup', array($this, 'text_for_section_eight'), 'reach-connector-options' );
+      add_settings_section('section-nine', 'Place Page Shortcode Setup', array($this, 'text_for_section_nine'), 'reach-connector-options' );
     }
 
     // Hook for WordPress admin_init action
@@ -121,7 +127,7 @@ if(!class_exists('Reach_Connector_Plugin')) {
     }
 
     public function text_for_section_one() {
-    	echo "Enter your REACH&#8480; Admin URL and Account GUID to setup the REACH&#8480; Connector plugin.";
+    	echo "Enter your REACH&#8480; Account URL and Account GUID to setup the REACH&#8480; Connector plugin.";
     }
     
     public function text_for_style_section() {
@@ -129,7 +135,7 @@ if(!class_exists('Reach_Connector_Plugin')) {
     }
     
     public function text_for_section_two() {
-    	echo "To pull a list of sponsorships from REACH&#8480; to display on your site use the shortcode [sponsorships]. You can also pass conditional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:";
+    	echo "To pull a list of sponsorships from REACH&#8480; to display on your site use the shortcode [sponsorships]. You can also pass optional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:";
       echo "<p><blockquote>sponsorship_type<br/>location<br/>project<br/>sponsorship_categories<br/>status</blockquote></p>";
       echo '<p>Example: [sponsorships sponsorship_type="children"]';
     }
@@ -137,7 +143,32 @@ if(!class_exists('Reach_Connector_Plugin')) {
     public function text_for_section_three() {
     	echo "To pull a list of campaigns from REACH&#8480; to display on your site use the shortcode [campaigns].";
     }
-
+    
+    public function text_for_section_four() {
+    	echo "To pull a specific campaign from REACH&#8480; to display on your site use the shortcode with the permalink shown in REACH&#8480; [campaign permalink='my-campaign-permalink'].";
+    }
+    
+    public function text_for_section_five() {
+    	echo "To display a donation page from REACH&#8480; on your site use the shortcode [donations] on any page. You can also pass optional paramters defined in the Giving Options page in REACH to customize the donation form by setting amount, recurring period, purpose etc.";
+      echo "<p><blockquote>amount<br/>fixed_amount (true/false)<br/>recurring<br/>fixed_recurring (true/false)<br/>referral</blockquote></p>";
+      echo '<p>Example: [donations amount="50"]';
+    }
+    
+    public function text_for_section_six() {
+    	echo "To pull a list of projects from REACH&#8480; to display on your site use the shortcode [projects].";
+    }
+    
+    public function text_for_section_seven() {
+    	echo "To pull a specific project from REACH&#8480; to display on your site use the shortcode with the permalink shown in REACH&#8480; [project permalink='my-project-permalink'].";
+    }
+    
+    public function text_for_section_eight() {
+    	echo "To pull a list of places from REACH&#8480; to display on your site use the shortcode [places].";
+    }
+    
+    public function text_for_section_nine() {
+    	echo "To pull a specific place from REACH&#8480; to display on your site use the shortcode with the permalink shown in REACH&#8480; [place permalink='my-place-permalink'].";
+    }
   }
 }
 
@@ -168,12 +199,13 @@ function get_sponsorships($atts) {
       'project' => '',
       'sponsorship_categories' => '',
       'status' => '',
+      'hide_filters' => '',
       'disablenav' => 'true',
   ), $atts, 'sponsorships' );
   echo "<iframe id='sponsorships-iframe' src='https://".str_replace($search, '', $reach_api_host)."/sponsorships?".http_build_query($atts)."' width='100%' height='2000px' scrolling='no' frameborder='0'></iframe>";
   echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
   echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
-  echo '<script>$("#sponsorships-iframe").iFrameResize();</script>';
+  echo '<script>$("#sponsorships-iframe").iFrameResize({checkOrigin:false});</script>';
   echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
 }
 
@@ -185,12 +217,105 @@ function get_campaigns() {
   echo "<iframe id='campaigns-iframe' src='https://".str_replace($search, '', $reach_api_host)."/campaigns?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
   echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
   echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
-  echo '<script>$("#campaigns-iframe").iFrameResize();</script>';
+  echo '<script>$("#campaigns-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_projects() {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  echo "<iframe id='projects-iframe' src='https://".str_replace($search, '', $reach_api_host)."/projects?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#projects-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_places() {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  echo "<iframe id='places-iframe' src='https://".str_replace($search, '', $reach_api_host)."/places?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#places-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_project_page($atts) {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $atts = shortcode_atts( array(
+      'permalink' => '',
+      'disablenav' => 'true',
+  ), $atts, 'sponsorships' );
+  echo "<iframe id='projects-iframe' src='https://".str_replace($search, '', $reach_api_host)."/projects/".$atts['permalink']."?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#projects-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_place_page($atts) {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $atts = shortcode_atts( array(
+      'permalink' => '',
+      'disablenav' => 'true',
+  ), $atts, 'sponsorships' );
+  echo "<iframe id='places-iframe' src='https://".str_replace($search, '', $reach_api_host)."/places/".$atts['permalink']."?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#places-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_campaign_page($atts) {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $atts = shortcode_atts( array(
+      'permalink' => '',
+      'disablenav' => 'true',
+  ), $atts, 'sponsorships' );
+  echo "<iframe id='campaigns-iframe' src='https://".str_replace($search, '', $reach_api_host)."/campaigns/".$atts['permalink']."?disablenav=true' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#campaigns-iframe").iFrameResize({checkOrigin:false});</script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
+}
+
+function get_donation_page($atts) {
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $reach_campaign_class = esc_attr( get_option( 'reach_campaign_class' ) );
+  $search  = array('https://', 'http://');
+  $reach_api_host = esc_attr( get_option( 'reach_api_host' ) );
+  $atts = shortcode_atts( array(
+      'amount' => '',
+      'fixed_amount' => '',
+      'recurring' => '',
+      'fixed_recurring' => '',
+      'referral' => '',
+      'disablenav' => 'true',
+  ), $atts, 'sponsorships' );
+  echo "<iframe id='donations-iframe' src='https://".str_replace($search, '', $reach_api_host)."/donations/new?".http_build_query($atts)."' width='100%' scrolling='no' frameborder='0'></iframe>";
+  echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>';
+  echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.min.js'></script>";
+  echo '<script>$("#donations-iframe").iFrameResize({checkOrigin:false});</script>';
   echo "<script type='text/javascript' src='https://".str_replace($search, '', $reach_api_host)."/assets/iframeResizer.contentWindow.min.js'></script>";
 }
 
 add_shortcode('sponsorships', 'get_sponsorships');
 add_shortcode('campaigns', 'get_campaigns');
+add_shortcode('campaign', 'get_campaign_page');
+add_shortcode('projects', 'get_projects');
+add_shortcode('project', 'get_project_page');
+add_shortcode('places', 'get_places');
+add_shortcode('place', 'get_place_page');
+add_shortcode('donations', 'get_donation_page');
 
 if(class_exists('Reach_Connector_Plugin')) {
   // WordPress hooks to activate and deactivate the plugin
--- readme.txt
+++ readme.txt
@@ -1,46 +1,89 @@
 === reach-connector ===
 Contributors: David McWilliams, Phi Sanders
-Tags: reach, connector, campaign, charity, child, sponorships, fundraising, donate, sugar, maple, interactive
+Tags: reach, connector, campaign, charity, child, sponsorships, fundraising, donate, sugar, maple, interactive
 Requires at least: 3.1
-Tested up to: 4.1.1
-Stable tag: 0.2
+Tested up to: 4.2.3
+Stable tag: 1.6
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin enables you to easily integrate your REACH campaigns and sponsorships with your WordPress site.
+This plugin enables you to easily integrate your REACH&#8480; campaigns and sponsorships with your WordPress site.
 
 == Description ==
-This plugin enables you to easily integrate your REACH with your WordPress site. Use short codes to embed all sponsorships or campaigns on pages or filter based on short code parameters.
+This plugin enables you to easily integrate your REACH&#8480; site with your WordPress site. Use short codes to embed all sponsorships or campaigns on pages or filter based on short code parameters.
 
 == Installation ==
-Copy the \"reach-connector\" folder and all of it\'s files in to your \"wp-content/plugins\" folder.
-
-Edit the look of each widget by modifying the HTML attributes in reach-connector.php in the methods get_sponsorships and get_campaigns.
+Copy the "reach-connector" folder and all of it's files in to your "wp-content/plugins" folder.
 
 == Frequently Asked Questions ==
 = Does this plugin require a REACH account? =
 Yes, you can sign up for a REACH account at http://www.reachapp.co.
 
+== Support ==
+Please visit http://support.reachapp.co or email support@reachapp.co.
+
 == Upgrade Notice ==
-= 0.2 =
+= 1.3 =
 Please upgrade to the latest version to enable new enhancements to the plugin.
 
 == Changelog ==
-= 0.1 =
+= 1.1 =
 * This is the intial release to the public.
 
-= 0.2 =
+= 1.2 =
 * This release switches to a more user-friendly format which provides pagination and filtering for sponsorships and campaigns.
 
-== Usage ==
-To pull a list of sponsorships from REACH to display on your site use the shortcode [sponsorships] on any page. You can also pass conditional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:
+= 1.3 =
+* This release adds the new shortcode "donations" to display the general donation form on any page.
 
+= 1.3.1 =
+* Added documentation to simplify the setup process.
+
+= 1.4 =
+* This release adds optional parameters to customize the donations shortcode.
+
+= 1.5 =
+* This release adds shortcodes for projects, places, and individual pages for projects, places, and campaigns.
+
+= 1.6 =
+* Bug fix when accounts are using their own sub-domain on REACH.
+
+== Usage ==
+Sponsorship Shortcode Setup
+
+To pull a list of sponsorships from REACH℠ to display on your site use the shortcode [sponsorships]. You can also pass optional parameters to filter your sponsorship results similar to the dropdown filters on the Sponsorships page using the parameters:
 sponsorship_type
 location
 project
 sponsorship_categories
 status
-
 Example: [sponsorships sponsorship_type="children"]
 
-To pull a list of campaigns from REACH to display on your site use the shortcode [campaigns] on any page.
+Campaigns Shortcode Setup
+
+To pull a list of campaigns from REACH℠ to display on your site use the shortcode [campaigns].
+Campaign Page Shortcode Setup
+
+To pull a specific campaign from REACH℠ to display on your site use the shortcode with the permalink shown in REACH℠ [campaign permalink='my-campaign-permalink'].
+Donation Shortcode Setup
+
+To display a donation page from REACH℠ on your site use the shortcode [donations] on any page. You can also pass optional paramters defined in the Giving Options page in REACH to customize the donation form by setting amount, recurring period, purpose etc.
+amount
+fixed_amount (true/false)
+recurring
+fixed_recurring (true/false)
+referral
+Example: [donations amount="50"]
+
+Projects Shortcode Setup
+
+To pull a list of projects from REACH℠ to display on your site use the shortcode [projects].
+Project Page Shortcode Setup
+
+To pull a specific project from REACH℠ to display on your site use the shortcode with the permalink shown in REACH℠ [project permalink='my-project-permalink'].
+Places Shortcode Setup
+
+To pull a list of places from REACH℠ to display on your site use the shortcode [places].
+Place Page Shortcode Setup
+
+To pull a specific place from REACH℠ to display on your site use the shortcode with the permalink shown in REACH℠ [place permalink='my-place-permalink'].
